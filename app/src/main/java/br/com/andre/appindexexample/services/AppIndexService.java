ff071495ea97a8a2ffe4a9f9e0c8937fc675fc3e
AndreFRSales <andrefernandes.sales@gmail.com>
2017-05-08 21:55:54 -0300

    Creating example for index

--- app/src/main/java/br/com/andre/appindexexample/services/AppIndexService.java
+++ app/src/main/java/br/com/andre/appindexexample/services/AppIndexService.java
@@ -2,10 +2,14 @@ package br.com.andre.appindexexample.services;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.Nullable;
 
+import com.google.firebase.appindexing.FirebaseAppIndex;
 import com.google.firebase.appindexing.Indexable;
+import com.google.firebase.appindexing.builders.Indexables;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
@@ -14,12 +18,39 @@ import java.util.ArrayList;
 
 public class AppIndexService extends IntentService {
 
-    public AppIndexService(String name) {
-        super(name);
+    private final Uri BASE_URL = Uri.parse("https://www.indexable.com/indexables/");
+
+    public AppIndexService() {
+        super(AppIndexService.class.getName());
     }
 
     @Override
     protected void onHandleIntent(@Nullable Intent intent) {
         ArrayList<Indexable> indexables = new ArrayList<>();
+
+        for(String text : getMockIndexables()) {
+            final String url = BASE_URL.buildUpon().appendPath(text).build().toString();
+            Indexable mockIndexable = Indexables.noteDigitalDocumentBuilder()
+                    .setUrl(url)
+                    .setName("Indexable List")
+                    .build();
+
+            indexables.add(mockIndexable);
+        }
+
+        Indexable[] indexableArray = new Indexable[indexables.size()];
+        indexableArray = indexables.toArray(indexableArray);
+
+        FirebaseAppIndex.getInstance().update(indexableArray);
+    }
+
+    private ArrayList<String> getMockIndexables(){
+        ArrayList<String> indexables = new ArrayList<>();
+
+        for(int count = 0; count < 10; count++){
+            indexables.add("index" + count);
+        }
+
+        return indexables;
     }
 }
